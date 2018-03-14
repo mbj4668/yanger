mbj4668/yanger

commit 836aa1e9dd07e4cef596be5804e04117ef3c30e6

Propagate if_feature evaluation to augmenting/deviating/annotating modules

src/yang.erl
@@ -2029,24 +2029,19 @@ mk_children([{Kwd, Arg, Pos, Substmts} = Stmt | T], GroupingMap0,
                    end,
             {FeatureL, WhenL, MustL, Status, Ctx1} =
                 common_substmts(Substmts, 'local', M, Ctx0),
+            IfFeatureRes = check_if_features(FeatureL, Ctx1),
             Sn0 = #sn{name = Name,
                       kind = Kind,
                       module = M,
                       typedefs = Typedefs,
                       groupings = Groupings,
                       if_feature = FeatureL,
+                      if_feature_result = IfFeatureRes,
                       'when' = WhenL,
                       must = MustL,
                       status = Status,
                       stmt = Stmt},
-            IfFeatureRes = check_if_features(FeatureL, Ctx1),
-            if IfFeatureRes == false ->
-                    %% skip this schema node
-                    mk_children(T, GroupingMap0, Typedefs, Groupings,
-                                ParentTypedefs, ParentGroupings,
-                                M, IsInGrouping, Ctx1,
-                                Mode, Ancestors, Acc, XAcc);
-               Kwd == 'leaf' orelse Kwd == 'leaf-list' ->
+            if Kwd == 'leaf' orelse Kwd == 'leaf-list' ->
                     {Ctx2, Sn1} =
                         run_mk_sn_hooks(Ctx1, Sn0, #hooks.pre_mk_sn, Mode,
                                         undefined, Ancestors),
@@ -2108,35 +2103,29 @@ mk_children_uses(Grouping, UsesStmt, GroupingMap, Typedefs, Groupings, M,
     {_, _, Pos, Substmts} = UsesStmt,
     {FeatureL, WhenL, [] = _MustL, _Status, Ctx1} =
         common_substmts(Substmts, 'uses', M, Ctx0),
-    case check_if_features(FeatureL, Ctx1) of
-        true ->
-            GroupingChildren1 =
-                if FeatureL /= [] orelse WhenL /= [] ->
-                        [Ch#sn{if_feature = FeatureL ++ Ch#sn.if_feature,
-                               'when' = WhenL ++ Ch#sn.'when'} ||
-                            Ch <- Grouping#grouping.children];
-                   true ->
-                        Grouping#grouping.children
-                end,
-            {ExpandedChildren, Ctx2} =
-                expand_uses(GroupingChildren1, Substmts,
-                            Typedefs, Groupings, Pos, M, Ctx1, Mode, Ancestors),
-            %% possibly copy stms from the grouping
-            CopyMap = (Ctx0#yctx.env)#env.copy_from_grouping_stmts,
-            CopySubstmts = [S || S <- stmt_substmts(Grouping#grouping.stmt),
-                                 map_is_key(stmt_keyword(S), CopyMap)],
-            mk_children(RestStmts, GroupingMap,
-                        Typedefs, Groupings,
-                        Typedefs, Groupings, M,
-                        IsInGrouping, Ctx2, Mode, Ancestors,
-                        ExpandedChildren ++ Acc, XAcc ++ CopySubstmts);
-        false ->
-            mk_children(RestStmts, GroupingMap,
-                        Typedefs, Groupings,
-                        Typedefs, Groupings, M,
-                        IsInGrouping, Ctx1, Mode, Ancestors,
-                        Acc, XAcc)
-    end.
+    IfFeatureRes = check_if_features(FeatureL, Ctx1),
+    GroupingChildren1 =
+        if FeatureL /= [] orelse WhenL /= [] ->
+                [Ch#sn{if_feature = FeatureL ++ Ch#sn.if_feature,
+                       if_feature_result =
+                           IfFeatureRes and Ch#sn.if_feature_result,
+                       'when' = WhenL ++ Ch#sn.'when'} ||
+                    Ch <- Grouping#grouping.children];
+           true ->
+                Grouping#grouping.children
+        end,
+    {ExpandedChildren, Ctx2} =
+        expand_uses(GroupingChildren1, Substmts,
+                    Typedefs, Groupings, Pos, M, Ctx1, Mode, Ancestors),
+    %% possibly copy stms from the grouping
+    CopyMap = (Ctx0#yctx.env)#env.copy_from_grouping_stmts,
+    CopySubstmts = [S || S <- stmt_substmts(Grouping#grouping.stmt),
+                         map_is_key(stmt_keyword(S), CopyMap)],
+    mk_children(RestStmts, GroupingMap,
+                Typedefs, Groupings,
+                Typedefs, Groupings, M,
+                IsInGrouping, Ctx2, Mode, Ancestors,
+                ExpandedChildren ++ Acc, XAcc ++ CopySubstmts).
 
 expand_uses(GroupingChildren, UsesSubstmts, Typedefs, Groupings,
             UsesPos, M, Ctx, Mode, Ancestors) ->
@@ -3156,42 +3145,39 @@ mk_augments([{'augment', Arg, Pos, Substmts} = Stmt | T], Typedefs, Groupings,
             M, Ctx0, IsTopLevel, Acc) ->
     {FeatureL, WhenL, [] = _MustL, _Status, Ctx1} =
         common_substmts(Substmts, 'augment', M, Ctx0),
-    case check_if_features(FeatureL, Ctx1) of
-        true ->
-            {Children0, _, _, Ctx2} =
-                %% Create the children w/o explicit config property.
-                %% The #sn{} nodes will get the correct config later when
-                %% they are inserted into the tree.
-                mk_children(Substmts, undefined, Typedefs, Groupings,
-                            undefined, undefined,
-                            M, _IsInGrouping = false, Ctx1,
-                            _Mode = augment, _Ancestors = [], _Acc = [], []),
-            Ctx3 = v_unique_names(Children0, Ctx2),
-            case parse_schema_nodeid(IsTopLevel, Arg, Pos, M, Ctx3) of
-                {ok, SchemaNodeId, Ctx4} ->
-                    %% Propagate when/if-feature from the augment statement
-                    %% to the children. (must is not allowed in augment)
-                    Children1 =
-                        if FeatureL /= [] orelse WhenL /= [] ->
-                                [Ch#sn{if_feature = FeatureL ++
-                                           Ch#sn.if_feature,
-                                       'when' = WhenL ++ Ch#sn.'when'}
-                                 || Ch <- Children0];
-                           true ->
-                                Children0
-                        end,
-                    Aug = #augment{target_node = SchemaNodeId,
-                                   stmt = Stmt,
-                                   has_when = WhenL /= [],
-                                   children = Children1},
-                    mk_augments(T, Typedefs, Groupings, M, Ctx4, IsTopLevel,
-                                [Aug | Acc]);
-                {error, Ctx4} ->
-                    mk_augments(T, Typedefs, Groupings, M, Ctx4, IsTopLevel,
-                                Acc)
-            end;
-        false ->
-            mk_augments(T, Typedefs, Groupings, M, Ctx1, IsTopLevel, Acc)
+    IfFeatureRes = check_if_features(FeatureL, Ctx1),
+    {Children0, _, _, Ctx2} =
+        %% Create the children w/o explicit config property.
+        %% The #sn{} nodes will get the correct config later when
+        %% they are inserted into the tree.
+        mk_children(Substmts, undefined, Typedefs, Groupings,
+                    undefined, undefined,
+                    M, _IsInGrouping = false, Ctx1,
+                    _Mode = augment, _Ancestors = [], _Acc = [], []),
+    Ctx3 = v_unique_names(Children0, Ctx2),
+    case parse_schema_nodeid(IsTopLevel, Arg, Pos, M, Ctx3) of
+        {ok, SchemaNodeId, Ctx4} ->
+            %% Propagate when/if-feature from the augment statement
+            %% to the children. (must is not allowed in augment)
+            Children1 =
+                if FeatureL /= [] orelse WhenL /= [] ->
+                        [Ch#sn{if_feature = FeatureL ++ Ch#sn.if_feature,
+                               if_feature_result =
+                                   IfFeatureRes and Ch#sn.if_feature_result,
+                               'when' = WhenL ++ Ch#sn.'when'}
+                         || Ch <- Children0];
+                   true ->
+                        Children0
+                end,
+            Aug = #augment{target_node = SchemaNodeId,
+                           stmt = Stmt,
+                           has_when = WhenL /= [],
+                           children = Children1},
+            mk_augments(T, Typedefs, Groupings, M, Ctx4, IsTopLevel,
+                        [Aug | Acc]);
+        {error, Ctx4} ->
+            mk_augments(T, Typedefs, Groupings, M, Ctx4, IsTopLevel,
+                        Acc)
     end;
 mk_augments([_ | T], Typedefs, Groupings, M, Ctx, IsTopLevel, Acc) ->
     mk_augments(T, Typedefs, Groupings, M, Ctx, IsTopLevel, Acc);

include/yang.hrl
@@ -138,6 +138,7 @@
           if_feature = [] :: [{#feature{},
                                Origin :: 'uses' | 'augment' | 'local',
                                yang:stmt()}],
+          if_feature_result = true :: boolean(),
           augmented_by = [] :: [yang:augment_rec()],
           is_augment_top_node = false :: boolean(),
           stmt :: yang:stmt(), % pointer to raw statement

plugins/yanger_tree.erl
@@ -231,6 +231,9 @@ print_children(Children, Mod, Fd, PKey, Prefix, Path, Mode, Depth, Width) ->
 -spec print_node(#sn{}, #module{}, erlang:device(), 'undefined' | [atom()],
                  string(), undefined | [atom()], mode(),
                  integer(), integer()) -> ok.
+print_node(#sn{if_feature_result = false},
+           _Mod, _Fd, _PKey, _Prefix, _Path, _Mode, _Depth, _Width) ->
+    skip;
 print_node(Sn, Mod, Fd, PKey, Prefix, Path, Mode, Depth, Width) ->
     {KW, StmtArg, _, Subs} = Sn#sn.stmt,
     TypeName = typename(Sn#sn.type, Mod),

commit 1694ab9696c2e0774ba5e68532d73fa53d886e67

Fix error/warning reporting for references to/from if-feature-false nodes
Bug introduced by 836aa1e9 - need to filter out nodes that
have #sn{if_feature_result = false}. Done also for pre/post_mk_sn hooks.

src/yang.erl
@@ -2442,6 +2442,9 @@ common_substmts([_ | T], Origin, M, Ctx, FeatureL, WhenL, MustL, Status) ->
 common_substmts([], _Origin, _M, Ctx, FeatureL, WhenL, MustL, Status) ->
     {FeatureL, WhenL, MustL, Status, Ctx}.
 
+run_mk_sn_hooks_rec(Ctx, #sn{if_feature_result = false} = Sn,
+                    _Mode, _UsesPos, _Ancestors) ->
+    {Ctx, Sn};
 run_mk_sn_hooks_rec(Ctx0, Sn0, Mode, UsesPos, Ancestors0)
   when Sn0#sn.kind /= '__tmp_augment__' ->
     {Ctx1, Sn1} =
@@ -2461,6 +2464,9 @@ run_mk_sn_hooks_rec(Ctx, Sn, _Mode, _UsesPos, _Ancestors) ->
 
 %run_mk_sn_hooks(Ctx, Sn, _HookField, _Mode = augment, _, _) ->
 %    {Ctx, Sn};
+run_mk_sn_hooks(Ctx, #sn{if_feature_result = false} = Sn, _HookField,
+                _Mode, _UsesPos, _Ancestors) ->
+    {Ctx, Sn};
 run_mk_sn_hooks(#yctx{hooks = Hooks} = Ctx0, Sn0, HookField,
                 Mode, UsesPos, Ancestors) ->
     %% NOTE: foldr is important here in order to run the built-in hooks
@@ -3795,6 +3801,8 @@ post_expand_module(#module{children = Children0, typedefs = Typedefs} = M,
 post_expand_module(_M, Ctx) ->
     Ctx.
 
+post_expand_child(#sn{if_feature_result = false}, _Ancestors, _M, Ctx) ->
+    Ctx;
 post_expand_child(#sn{module = #module{modulename = ModName,
                                        modulerevision = ModRev},
                       kind = Kind,
@@ -4568,6 +4576,9 @@ cursor_set_sn(#sn{module = SnMod} = Sn, #cursor{module = CurM} = C, Ctx) ->
 find_child(Sns, Id) ->
     find_child(Sns, Id, schema, '$undefined', undefined).
 
+find_child([#sn{if_feature_result = false} | T],
+            Id, Type, InitMod, LastSkipped) ->
+    find_child(T, Id, Type, InitMod, LastSkipped);
 find_child([#sn{kind = Kind, children = Chs} = H | T] = Sns,
            Id, Type, InitMod, LastSkipped) ->
     %% If we're in the data tree, we may need to check non-data nodes'

test/lux/bad/no-feat-ref-ext.yang
@@ -1,0 +1,52 @@
+module no-feat-ref-ext {
+  namespace urn:no-feat-ref-ext;
+  prefix nfre;
+
+  // YANGER_EXTRA_FLAGS: -F no-feat-ref:bar
+
+  import no-feat-ref {
+    prefix nfr;
+  }
+
+  leaf ref1 {
+    type leafref {
+      path /nfr:rootleaf; // LINE: YANG_ERR_NODE_NOT_FOUND2
+    }
+  }
+  leaf ref2 {
+    type leafref {
+      path /nfr:rootlist/nfr:id; // LINE: YANG_ERR_NODE_NOT_FOUND2
+    }
+  }
+  leaf ref3 {
+    type leafref {
+      path /nfr:rootcont/nfr:xxx; // LINE: YANG_ERR_NODE_NOT_FOUND3
+    }
+  }
+
+  leaf must1 {
+    type int16;
+    must ". > /nfr:rootleaf"; // LINE: YANG_ERR_XPATH_BAD_REF
+  }
+  leaf must2 {
+    type int16;
+    must ". > /nfr:rootlist/nfr:id"; // LINE: YANG_ERR_XPATH_BAD_REF
+  }
+  leaf must3 {
+    type int16;
+    must ". > /nfr:rootcont/nfr:xxx"; // LINE: YANG_ERR_XPATH_BAD_REF
+  }
+
+  leaf when1 {
+    when "../must1 > /nfr:rootleaf"; // LINE: YANG_ERR_XPATH_BAD_REF
+    type int16;
+  }
+  leaf when2 {
+    when "../must1 > /nfr:rootlist/nfr:id"; // LINE: YANG_ERR_XPATH_BAD_REF
+    type int16;
+  }
+  leaf when3 {
+    when "../must1 > /nfr:rootcont/nfr:xxx"; // LINE: YANG_ERR_XPATH_BAD_REF
+    type int16;
+  }
+}

test/lux/bad/no-feat-ref.yang
@@ -1,0 +1,233 @@
+module no-feat-ref {
+  namespace urn:no-feat-ref;
+  prefix nfr;
+
+  // YANGER_EXTRA_FLAGS: -F no-feat-ref:bar
+
+  feature foo;
+  feature bar;
+
+  leaf rootleaf {
+    if-feature foo;
+    type int32;
+  }
+
+  list rootlist {
+    if-feature foo;
+    key id;
+    leaf id {
+      type uint32;
+    }
+  }
+
+  container rootcont {
+    leaf xxx {
+      if-feature foo;
+      type uint32;
+    }
+    choice ch1 {
+      if-feature foo;
+      leaf ca {
+        type int32;
+      }
+    }
+    choice ch2 {
+      case ca1 {
+        if-feature foo;
+        leaf ca1 {
+          type int32;
+        }
+      }
+    }
+    // these should give errors/warnings if foo is not defined
+    leaf ref {
+      type leafref {
+        path ../xxx; // LINE: YANG_ERR_NODE_NOT_FOUND3
+      }
+    }
+    leaf must {
+      type int16;
+      must ". > ../xxx"; // LINE: YANG_ERR_XPATH_BAD_REF
+    }
+    leaf when {
+      when "../must > ../xxx"; // LINE: YANG_ERR_XPATH_BAD_REF
+      type int16;
+    }
+    leaf chref {
+      type leafref {
+        path ../ca; // LINE: YANG_ERR_NODE_NOT_FOUND3
+      }
+    }
+    leaf camust {
+      type int16;
+      must ". > ../ca1"; // LINE: YANG_ERR_XPATH_BAD_REF
+    }
+    // these should *not* give errors/warnings if foo is not defined
+    leaf good-ref {
+      if-feature foo;
+      type leafref {
+        path ../xxx;
+      }
+    }
+    leaf good-must {
+      if-feature foo;
+      type int16;
+      must ". > ../xxx";
+    }
+    leaf good-when {
+      if-feature foo;
+      when "../must > ../xxx";
+      type int16;
+    }
+    container good {
+      if-feature foo;
+      leaf ref {
+        type leafref {
+          path ../../xxx;
+        }
+      }
+      leaf must {
+        type int16;
+        must ". > ../../xxx";
+      }
+      leaf when {
+        when "../../must > ../../xxx";
+        type int16;
+      }
+    }
+    choice ch3 {
+      if-feature foo;
+      leaf chcaref {
+        type leafref {
+          path ../xxx;
+        }
+      }
+    }
+    choice ch4 {
+      case ca1 {
+        if-feature foo;
+        leaf chca1when {
+          when "../must > ../xxx";
+          type int16;
+        }
+      }
+    }
+  }
+
+  // these should give errors/warnings if foo is not defined
+  leaf ref1 {
+    type leafref {
+      path /rootleaf; // LINE: YANG_ERR_NODE_NOT_FOUND2
+    }
+  }
+  leaf ref2 {
+    type leafref {
+      path /rootlist/id; // LINE: YANG_ERR_NODE_NOT_FOUND2
+    }
+  }
+  leaf ref3 {
+    type leafref {
+      path /rootcont/xxx; // LINE: YANG_ERR_NODE_NOT_FOUND3
+    }
+  }
+  leaf ref4 {
+    type leafref {
+      path ../rootleaf; // LINE: YANG_ERR_NODE_NOT_FOUND2
+    }
+  }
+  leaf ref5 {
+    type leafref {
+      path ../rootcont/xxx; // LINE: YANG_ERR_NODE_NOT_FOUND3
+    }
+  }
+  // these should *not* give errors/warnings if foo is not defined
+  leaf good-ref {
+    if-feature foo;
+    type leafref {
+      path /rootleaf;
+    }
+  }
+  list good1 {
+    if-feature foo;
+    key ref2;
+    leaf ref2 {
+      type leafref {
+        path /rootlist/id;
+      }
+    }
+  }
+
+  // these should give errors/warnings if foo is not defined
+  leaf must1 {
+    type int16;
+    must ". > /rootleaf"; // LINE: YANG_ERR_XPATH_BAD_REF
+  }
+  leaf must2 {
+    type int16;
+    must ". > /rootlist/id"; // LINE: YANG_ERR_XPATH_BAD_REF
+  }
+  leaf must3 {
+    type int16;
+    must ". > /rootcont/xxx"; // LINE: YANG_ERR_XPATH_BAD_REF
+  }
+  leaf must4 {
+    type int16;
+    must ". > ../rootleaf"; // LINE: YANG_ERR_XPATH_BAD_REF
+  }
+  leaf must5 {
+    type int16;
+    must ". > ../rootcont/xxx"; // LINE: YANG_ERR_XPATH_BAD_REF
+  }
+  // these should *not* give errors/warnings if foo is not defined
+  leaf good-must {
+    if-feature foo;
+    type int16;
+    must ". > ../rootleaf";
+  }
+  container good2 {
+    if-feature foo;
+    leaf must5 {
+      type int16;
+      must ". > ../../rootcont/xxx";
+    }
+  }
+
+  // these should give errors/warnings if foo is not defined
+  leaf when1 {
+    when "../must1 > /rootleaf"; // LINE: YANG_ERR_XPATH_BAD_REF
+    type int16;
+  }
+  leaf when2 {
+    when "../must1 > /rootlist/id"; // LINE: YANG_ERR_XPATH_BAD_REF
+    type int16;
+  }
+  leaf when3 {
+    when "../must1 > /rootcont/xxx"; // LINE: YANG_ERR_XPATH_BAD_REF
+    type int16;
+  }
+  leaf when4 {
+    when "../must1 > ../rootleaf"; // LINE: YANG_ERR_XPATH_BAD_REF
+    type int16;
+  }
+  leaf when5 {
+    when "../must1 > ../rootcont/xxx"; // LINE: YANG_ERR_XPATH_BAD_REF
+    type int16;
+  }
+  // these should *not* give errors/warnings if foo is not defined
+  leaf good-when {
+    if-feature foo;
+    when "../must1 > /rootcont/xxx";
+    type int16;
+  }
+  list good3 {
+    key k;
+    leaf k {
+      type string;
+    }
+    leaf when4 {
+      if-feature foo;
+      when "../../must1 > ../../rootleaf";
+      type int16;
+    }
+  }
+}