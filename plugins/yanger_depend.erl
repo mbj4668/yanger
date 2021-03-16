5f06819f1ed2ccccdab3559c89afda368f9e2d75
pmerlo <pmerlo@cisco.com>
2021-03-15 21:31:26 -0400

    Initial commit

--- plugins/yanger_depend.erl
+++ plugins/yanger_depend.erl
@@ -1,6 +1,8 @@
 -module(yanger_depend).
 -behaviour(yanger_plugin).
 
+-import(lists,[merge/1]).
+-import(lists,[usort/1]).
 -export([init/1]).
 
 -include_lib("yanger/include/yang.hrl").
@@ -18,6 +20,8 @@ option_specs() ->
         "Makefile rule target"},
        {depend_no_submodules, undefined, "depend-no-submodules", boolean,
         "Do not generate dependencies for included submodules"},
+       {depend_recurse, undefined, "depend-recurse", boolean,
+        "Generate dependencies to all imports recursively"},
        {depend_extension, undefined, "depend-extension", string,
         "YANG module file name extension"},
        {depend_include_path, undefined, "depend-include-path", boolean,
@@ -41,7 +45,7 @@ emit_mods(Ctx, Opts, [M | T], Fd) ->
             true -> "::";
             false -> ":"
         end,
-    PreReqs = get_prereqs(Opts, M),
+    PreReqs = get_prereqs(Ctx, Opts, M, []),
     io:format(Fd, "~s ~s", [DepTgt, ColonStr]),
     emit_prereqs(Ctx, Opts, PreReqs, Fd),
     emit_mods(Ctx, Opts, T, Fd);
@@ -79,7 +83,7 @@ emit_prereqs(Ctx, Opts, [PreReq | T], Fd) ->
 emit_prereqs(_Ctx, _Opts, [], Fd) ->
     io:format(Fd, "~n", []).
 
-get_prereqs(Opts, #module{stmt = Stmt}) ->
+get_prereqs(Ctx, Opts, #module{stmt = Stmt}, PreReqs) ->
     Imports = yang:search_all_stmts('import', Stmt),
     Includes =
         case proplists:get_value(depend_no_submodules, Opts, false) of
@@ -88,4 +92,11 @@ get_prereqs(Opts, #module{stmt = Stmt}) ->
             false ->
                 yang:search_all_stmts('include', Stmt)
         end,
-    [yang:stmt_arg(S) || S <- Imports ++ Includes].
+    NewPreReqs = [ T || T <- [yang:stmt_arg(S) || S <- Imports ++ Includes], lists:member(T, PreReqs) == false],
+    case proplists:get_value(depend_recurse, Opts, false) of
+        true ->
+            Models = [ yang:get_module(S, undefined, Ctx) || S <- NewPreReqs ], 
+            usort(PreReqs ++ merge([get_prereqs(Ctx, Opts, M, PreReqs ++ NewPreReqs) || {value, M}  <- Models]));
+        false ->
+            PreReqs ++ NewPreReqs
+    end.
